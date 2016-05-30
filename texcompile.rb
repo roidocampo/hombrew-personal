5ba8a79ccd373c5394861c9387511ee4f4493f85
Roi Docampo <roidocampo@com.fake>
2016-05-30 18:52:05 +0200

    update texcompile formula

--- texcompile.rb
+++ texcompile.rb
@@ -2,7 +2,7 @@ class Texcompile < Formula
     desc "Simple script for compilation of TeX/LaTeX files"
     homepage "https://github.com/roidocampo/texcompile"
     url "https://github.com/roidocampo/texcompile/archive/v0.1.tar.gz"
-    sha256 "3d2dadf0db364e0ab4f1d3e99f4b3dfd8539490047725c0f58f6f6ab0c05da7c"
+    sha256 "f4356c99df9f8012162f9d678a4806fe67e0231e529012bdb3feca6e92e95c60"
     version "0.1"
 
     depends_on "python"
