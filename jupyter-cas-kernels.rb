f07651d2d283fa046c7e0fe4002d019b5f243f54
Roi Docampo <roidocampo@com.fake>
2016-06-10 23:03:02 +0200

    update formula for jupyter-cas-kernels

--- jupyter-cas-kernels.rb
+++ jupyter-cas-kernels.rb
@@ -2,9 +2,9 @@ class JupyterCasKernels < Formula
 
     desc "Some kernels for jupyter"
     homepage "https://github.com/roidocampo/jupyter-cas-kernels"
-    url "https://github.com/roidocampo/jupyter-cas-kernels/archive/v0.1.tar.gz"
-    sha256 "c61905ae82dec7cde0534f71f996b954f4d90f70bc23116008aa6fd5529134c8"
-    version "0.1-rev1"
+    url "https://github.com/roidocampo/jupyter-cas-kernels/archive/v0.2.tar.gz"
+    sha256 "5dcf6795d74708efc650ef5016aac6a05a3579664cd3ce814dc14562c1d5dc02"
+    version "0.2"
 
     def install
         system "make", "install", "PREFIX=#{prefix}"
