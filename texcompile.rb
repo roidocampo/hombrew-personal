74f9b7dc5cb60836cac1eab5953f4ab1277240e6
Roi Docampo <roidocampo@com.fake>
2016-08-21 20:22:37 +0200

    updated texcompile to v0.3

--- texcompile.rb
+++ texcompile.rb
@@ -1,11 +1,11 @@
 class Texcompile < Formula
     desc "Simple script for compilation of TeX/LaTeX files"
     homepage "https://github.com/roidocampo/texcompile"
-    url "https://github.com/roidocampo/texcompile/archive/v0.2.tar.gz"
-    sha256 "a78571e6ccc2629a1c87210a088be7a6b22da8e6c0d0962f0c3c63ab0dbce1de"
-    version "0.2"
+    url "https://github.com/roidocampo/texcompile/archive/v0.3.tar.gz"
+    sha256 "93244bf9a994481eddde07f56b981255c94fa56efddfda3200584672315f8d1e"
+    version "0.3"
 
-    depends_on "python"
+    depends_on "python3"
 
     def install
         system "make", "install", "PREFIX=#{prefix}"
