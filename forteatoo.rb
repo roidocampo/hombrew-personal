c03834b4eb0b660f5110740b8c2c894c4377919b
Roi Docampo <roidocampo@gmail.com>
2016-11-30 18:41:48 -0600

    updated bottle for forteatoo

--- forteatoo.rb
+++ forteatoo.rb
@@ -8,6 +8,8 @@ class Forteatoo < Formula
     bottle do
         root_url 'https://raw.githubusercontent.com/roidocampo/homebrew-bottles/master'
         cellar :any
+        rebuild 1
+        sha256 "88aec0f40f01a0f69588c7fbbe7374c99419b566a54df48944fceb3348447457" => :el_capitan
         sha256 "1e05c6691320d37db870ef389a274ba7b65456e3761b0de334905af38239367c" => :yosemite
     end
 
