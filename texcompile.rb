bb271a27f1a07b2b79b1b3c46df90d0c0e401c1d
Roi Docampo <roidocampo@gmail.com>
2018-02-05 18:22:15 -0600

    fixed deprecated python3 dep

--- texcompile.rb
+++ texcompile.rb
@@ -4,8 +4,9 @@ class Texcompile < Formula
     url "https://github.com/roidocampo/texcompile/archive/v0.5.tar.gz"
     sha256 "c0aefcbf6305c43a0fd8c989235a724ba08be22d17e1ad20ef347bed640a22f6"
     version "0.5"
+    revision 1
 
-    depends_on :python3
+    depends_on "python3"
 
     include Language::Python::Virtualenv
 
